0f6abb8d2b1550401d16feca2c4dde2b1b09ce7f
Pampleto <sanchez.educr@gmail.com>
2026-08-05 17:34:36 -0600

    Feat: Sistema universitario completo (agregar una mejor interfaz

---
+++ Lab 02/src/modelo/Persona.java
@@ -0,0 +1,35 @@
+package modelo;
+import java.util.Objects;
+
+public abstract class Persona {
+    private String nombre;
+    private int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public int getEdad() { return edad; }
+    public void setEdad(int edad) { this.edad = edad; }
+    public abstract String getDescripcion();
+
+    @Override
+    public String toString() {
+        return "Persona{nombre='" + nombre + "', edad=" + edad + "}";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Persona persona = (Persona) obj;
+        return edad == persona.edad && Objects.equals(nombre, persona.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre, edad);
+    }
+}
